6526b52395827fe8d7720bc82649e5d9b27e783f
Evan <evangeorgemarshall@gmail.com>
2023-02-13 21:15:55 -0800

    Start adding back parallel

--- synthesizer/src/coinbase_puzzle/mod.rs
+++ synthesizer/src/coinbase_puzzle/mod.rs
@@ -41,6 +41,8 @@ use std::sync::Arc;
 
 #[cfg(feature = "parallel")]
 use rayon::prelude::*;
+#[cfg(not(feature = "parallel"))]
+use itertools::Itertools;
 
 #[derive(Clone)]
 pub enum CoinbasePuzzle<N: Network> {
@@ -211,12 +213,11 @@ impl<N: Network> CoinbasePuzzle<N> {
         // Construct the provers polynomial.
         let accumulated_prover_polynomial = cfg_into_iter!(prover_polynomials)
             .zip_eq(challenges)
-            .fold(DensePolynomial::zero, |mut accumulator, (mut prover_polynomial, challenge)| {
+            .fold(DensePolynomial::zero(), |mut accumulator, (mut prover_polynomial, challenge)| {
                 prover_polynomial *= challenge;
                 accumulator += &prover_polynomial;
                 accumulator
-            })
-            .sum::<DensePolynomial<_>>();
+            });
         let product_eval_at_challenge_point = accumulated_prover_polynomial.evaluate(accumulator_point)
             * epoch_challenge.epoch_polynomial().evaluate(accumulator_point);
 
@@ -312,10 +313,9 @@ impl<N: Network> CoinbasePuzzle<N> {
         // Compute the accumulator evaluation.
         let mut accumulator_evaluation = cfg_iter!(prover_polynomials)
             .zip_eq(&challenge_points)
-            .fold(<N::PairingCurve as PairingEngine>::Fr::zero, |accumulator, (prover_polynomial, challenge_point)| {
+            .fold(<N::PairingCurve as PairingEngine>::Fr::zero(), |accumulator, (prover_polynomial, challenge_point)| {
                 accumulator + (prover_polynomial.evaluate(accumulator_point) * challenge_point)
-            })
-            .sum();
+            });
         accumulator_evaluation *= &epoch_challenge.epoch_polynomial().evaluate(accumulator_point);
 
         // Compute the accumulator commitment.
